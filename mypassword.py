b12e0092b8d5172acf5bf7be4e4289b15f01d1b9
Pardlerum <pardlerum@gmail.com>
2016-09-14 19:53:02 +0100

    Remove public Salt - move to environment variable RegisterSalt

--- mypassword.py
+++ mypassword.py
@@ -1,12 +1,9 @@
 import hashlib
+import os
 
-
-# This is our secret 'salt' - must be same for all programs that need to check passwords
-# If this value is changed once passwords have been created they will all break and cannot be decoded!
-# (So don't change this value unless you REALLY know what you're doing - and keep a copy
-#  of the old value just in case...)
-# 67813abf79eb495bb3844501d50966cc
-oursalt = '67813abf79eb495bb3844501d50966cc'.encode('utf-8')
+# Salt is stored in an environment variable and must not change once passwords have been hashed
+# All systems accessing the passwords need the same salt
+oursalt = os.environ['RegisterSalt'].encode('utf-8')
 
 def GetPasswordHash(password):
     ''' Convert a password (string) into a 128 character hash value '''
